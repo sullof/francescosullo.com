9b138d2292b182aacd2c5941d5c5853a84310f2d
Francesco Sullo <sullof@gmail.com>
2025-11-04 13:24:50 +0100

    Improve the docker shell script

---
+++ docker.sh
@@ -0,0 +1,10 @@
+docker stop francescosullo-com 2>/dev/null || true
+docker rm francescosullo-com 2>/dev/null || true
+
+docker run --name francescosullo-com \
+  -p 8040 \
+  --restart unless-stopped \
+  -e VIRTUAL_HOST=francescosullo.com,www.francescosullo.com,sullo.co,francesco.sullo.co,www.sullo.co  \
+  -e LETSENCRYPT_HOST=francescosullo.com,www.francescosullo.com,sullo.co,francesco.sullo.co,www.sullo.co \
+  -e LETSENCRYPT_EMAIL=francescosullo@sameteam.co \
+  -v `pwd`/francescosullo.com/html:/usr/share/nginx/html:ro -d nginx
